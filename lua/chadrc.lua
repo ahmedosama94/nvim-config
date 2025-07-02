de00d8bb46cbc79ba26925e29e01456d5f620720
Ahmed Osama Ibrahim <ahmed.osama94@gmail.com>
2025-07-02 22:02:48 +0200

    update: added transparency

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -6,12 +6,13 @@
 local M = {}
 
 M.base46 = {
-	theme = "ayu_dark",
+  theme = "onedark",
+  transparency = true,
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  -- hl_override = {
+  --   Comment = { italic = true },
+  --   ["@comment"] = { italic = true },
+  -- },
 }
 
 -- M.nvdash = { load_on_startup = true }
